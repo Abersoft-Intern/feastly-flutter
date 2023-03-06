6a10a7350ab1a95c38b467134a0a2761e6a3a746
farkmu45 <farukmicros@outlook.com>
2023-03-06 19:25:40 +0700

    Added splash screen and launcher icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 // ignore:depend_on_referenced_packages
 import 'package:flutter_web_plugins/url_strategy.dart';
 
 import 'src/app.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
   usePathUrlStrategy();
+
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  FlutterNativeSplash.remove();
+
   runApp(const MyApp());
 }
