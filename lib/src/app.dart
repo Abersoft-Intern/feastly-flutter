9ef331cc02f6c32a49f6abb34b5d4fc9b3160696
farkmu45 <farukmicros@outlook.com>
2023-03-09 13:17:59 +0700

    Added welcome screen

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
       routerConfig: goRouter,
       restorationScopeId: 'app',
       theme: FeastlyTheme.light(),
-      darkTheme: FeastlyTheme.dark(),
+      darkTheme: FeastlyTheme.light(),
     );
   }
 }
