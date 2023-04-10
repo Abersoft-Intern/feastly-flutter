78834a156c7de7c6701426781e188749f2c79399
farkmu45 <farukmicros@outlook.com>
2023-04-10 15:43:57 +0700

    FEAS-111 Add disband group sheet

--- lib/src/presentation/groups/group/group_screen.dart
+++ lib/src/presentation/groups/group/group_screen.dart
@@ -6,6 +6,7 @@ import 'package:feastly/src/constants/theme/custom_color.dart';
 import 'package:feastly/src/constants/theme/custom_text_theme.dart';
 import 'package:feastly/src/localization/string_hardcoded.dart';
 import 'package:feastly/src/presentation/groups/group/member_avatar.dart';
+import 'package:feastly/src/utils/show_custom_bottom_sheet.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:share_plus/share_plus.dart';
@@ -163,7 +164,12 @@ class GroupScreen extends StatelessWidget {
                 Button(
                   text: 'Leave group'.hardcoded,
                   variant: ButtonVariant.danger,
-                  onTap: () {},
+                  onTap: () {
+                    showCustomBottomSheet(context,
+                        title: 'Disband Group'.hardcoded,
+                        subtitle: 'Are you sure you want to disband this group?'
+                            .hardcoded);
+                  },
                 ),
               ],
             ),
