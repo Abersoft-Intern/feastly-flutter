0618f7f81dbf0318512097ab1f5d4567bfcaf4d6
farkmu45 <farukmicros@outlook.com>
2023-04-13 13:50:19 +0700

    FEAS-121 Refactor main header

--- lib/src/common_widgets/main_header.dart
+++ lib/src/common_widgets/main_header.dart
@@ -36,21 +36,7 @@ class _MainHeaderState extends State<MainHeader> {
       children: [
         SmallButton(
           'Recipes'.hardcoded,
-          isSelected: _selectedMenu == 0,
           icon: FeastlyIcon.button_recipe_inactive,
-          onTap: () {
-            widget.onRecipesTap();
-            setState(() => _selectedMenu = 0);
-          },
-        ),
-        SmallButton(
-          'Restaurant'.hardcoded,
-          icon: FeastlyIcon.button_restaurant_inactive,
-          isSelected: _selectedMenu == 1,
-          onTap: () {
-            widget.onRestaurantTap();
-            setState(() => _selectedMenu = 1);
-          },
         ),
         const Spacer(),
         widget.child == null ? Container() : widget.child!
